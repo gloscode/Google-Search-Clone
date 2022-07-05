1a735d1fc3752a3df218d7ebaa722f9aa9ba4639
Aarav <agupta5046@gmail.com>
2022-07-05 18:39:02 +0530

    another commit to change script and style

--- script.js
+++ script.js
@@ -4,17 +4,18 @@ var search = document.querySelector(".search");
 var google = document.querySelector("#google");
 
 searchIcon.addEventListener("click", () => {
-  if (
-    input.value.includes(".") 
-  ) {
+  if (input.value.includes(".")) {
     open(`http://${input.value}`);
-  } else if (input.value.includes("http:") || input.value.includes("https:")) {
+  } 
+  else if (input.value.includes("http:") || input.value.includes("https:")) {
     open(`${input.value}`);
-  } else {
+  } 
+  else {
     if (input.value != "") {
       var url = `https://www.google.com/search?q=${input.value}`;
-      open(url, "_self");
-    } else {
+      open(url, "_blank");
+    } 
+    else {
     }
   }
 });
